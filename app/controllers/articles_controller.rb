d2782efe71d3b2bc32b4f3bb1c083e48b1996930
Santiago <santiagomorales333@yahoo.com>
2024-01-24 21:30:55 -0800

    added index and show routes for article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+    @test_val = 15
+  end
+
+  def show
+    # route parameters = parms[]
+    p params
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
+  
+  resources :articles
+  
 end
+# how to add a route
+  # get <route-name> to: <controller>#<controller_method>
+# resources :photos
+  # creates seven different routes in your application, 
+  # all mapping to the Photos controller:
+  # GET       /photos
+  # GET       /photos/new
+  # POST      /photos
+  # GET       /photos/:id
+  # GET       /photos/:id/edit
+  # PATCH/PUT /photos/:id
+  # DELETE    /photos/:id
